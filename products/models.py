e4e140317870048cf2d68641696966512fa5a447
Colin Shaw <colin.shaw400@outlook.com>
2023-06-25 14:09:18 +0000

    Add products model to models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#category model to seperate the products into categories
+class Category(models.Model):
+    #name of the catgory
+    name = models.CharField(max_length=254)
+    #friendly name of the category that will show up on the application
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
