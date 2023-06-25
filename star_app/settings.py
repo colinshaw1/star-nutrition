4adfac2fc67d1bbbb9f0718f5218ef583d51a3e6
Colin Shaw <colin.shaw400@outlook.com>
2023-06-25 12:07:12 +0000

    add products app and add products app to settings.py file

--- star_app/settings.py
+++ star_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'homepage',
+    'products',
     # adds allauth to installed apps
     'allauth',
     'allauth.account',
