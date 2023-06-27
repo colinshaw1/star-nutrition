87c3850124fc65aaccea6b4e48d7fc03ba3644ea
Colin Shaw <colin.shaw400@outlook.com>
2023-06-27 16:42:22 +0000

    Add view to view all products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def products_view(request):
+    """ A view to show all products, including sorting and search queries """
+
+    # return products form databse
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    # return on products.html page
+    return render(request, 'products/products.html', context)
