89e62040f1524e37551ce7f6667cc598df1254a1
Colin Shaw <colin.shaw400@outlook.com>
2023-06-27 18:32:03 +0000

    Add a view for viewing just a single products detaisl and a url for viewing the product detials

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 # view to call products   
 urlpatterns = [
     path('', views.products_view, name='products'),
+    # on product id return products details
+    path('<product_id>', views.product_details, name='product_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -12,5 +12,18 @@ def products_view(request):
     context = {
         'products': products,
     }
-    # return on products.html page
+    #return on products.html page
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ A view to single product details """
+
+    # taking in project id and getting object 404
+    product = get_object_or_40(product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    # return on product details
+    return render(request, 'products/product_details.html', context)
