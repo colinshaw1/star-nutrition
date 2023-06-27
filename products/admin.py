f4356f616c093d5fb1f95a6bc7b958c5895cff9f
Colin Shaw <colin.shaw400@outlook.com>
2023-06-27 16:10:03 +0000

    Add registering the products and categories models in admin.py file

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+#import model for products and categories
+from .models import Product, Category
 
 # Register your models here.
+#registering the models for products and categories
+admin.site.register(Product)
+admin.site.register(Category)
