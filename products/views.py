f3bd39566f02f7f5849e7389282438fc73b2c4c0
Colin Shaw <colin.shaw400@outlook.com>
2023-06-28 16:33:21 +0000

    Add search filters to views in product app

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -8,9 +10,28 @@ def products_view(request):
 
     # return products form databse
     products = Product.objects.all()
+    #stops an error wehn loading products page without a search term
+    query = None
+
+    # method taken from course content
+    # checking if requests. GET exists
+    if request.GET:
+        # if q is in request.GET equal query
+        if 'q' in request.GET:
+            query = request.GET['q']
+            # if q is in request.GET equal error message
+            if not query:
+                messages.error(request, "Please check your search query and try agian?")
+                return redirect(reversal('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            # passing to filter message to filter products
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        # query added to context
+        'search_term' : query,
     }
     #return on products.html page
     return render(request, 'products/products.html', context)
