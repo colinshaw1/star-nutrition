cf71a1d728f3d135df7960c847cb806f3c965558
Colin Shaw <colin.shaw400@outlook.com>
2023-06-30 16:25:32 +0000

    Add url for shopping bag

--- star_app/urls.py
+++ star_app/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path('', include('homepage.urls')),
     # adding products urls to project level file
     path('products/', include('products.urls')),
+    # adding shoppingbag urls to project level file
+    path('shoppingbag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #using static function to add url to media urls
