1bb68609d6cd6ea39853b57e2fc315d32cee8da3
Colin Shaw <colin.shaw400@outlook.com>
2023-07-05 16:49:06 +0000

    Add view for updating quantity of shopping bag, url for adding to bag and css for submit button

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,21 @@ def view_bag(request):
     """ A view to return the bag content page"""
 
     return render(request, 'bag/bag.html')
+
+# add in add to bag view 
+def add_to_bag(request, item_id):
+    """Add quanity of a product to the shopping bag"""
+    # get quanity and convert it to an integar as it is a string
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    # stores shopping bag in the sessions so it is not lost till the session is closed
+    bag =request.session.get('bag', {})
+
+    # add item to bag or update the bag quanity if already in bag
+    if item_id in list(bag.keys()):
+        bag[item_id] = quantity
+    else:
+        bag[item_id] = quantity
+    
+    # overwrite the variable if it doesnt exisit
+    request.session['bag'] = bag
