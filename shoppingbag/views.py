a93551c8ebee9505cb1706fe4895b0d1037d57f3
Colin Shaw <colin.shaw400@outlook.com>
2023-07-05 16:52:35 +0000

    Add redirect url and update view for shopping bag

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -14,13 +14,14 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     # stores shopping bag in the sessions so it is not lost till the session is closed
-    bag =request.session.get('bag', {})
+    bag = request.session.get('bag', {})
 
     # add item to bag or update the bag quanity if already in bag
     if item_id in list(bag.keys()):
-        bag[item_id] = quantity
+        bag[item_id] += quantity
     else:
         bag[item_id] = quantity
     
     # overwrite the variable if it doesnt exisit
     request.session['bag'] = bag
+    return redirect(redirect_url)
