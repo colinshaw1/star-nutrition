be646aed43d74fe0536c55c4f4c6c5b3af4a59f1
Colin Shaw <colin.shaw400@outlook.com>
2023-07-06 17:48:22 +0000

    Add updates to view for updating ba for sizes

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -45,19 +45,34 @@ def add_to_bag(request, item_id):
 
 # view update product quantity in the shopping bag
 def update_bag(request, item_id):
-    """Update the quantity of the items in the shopping bag"""
+    """ update quantity of a product to the shopping bag"""
     # get quanity and convert it to an integar as it is a string
     quantity = int(request.POST.get('quantity'))
+    # set size to none
+    size = None
+    #if statement for product size in request . post it gets set to that
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # stores shopping bag in the sessions so it is not lost till the session is closed
     bag = request.session.get('bag', {})
 
-    # update the bag quanity if already in bag
-    if quantity > 0:
-        bag[item_id] = quantity
-    else:
-        del bag[item_id]
-    
+    # if statment for sturcture of bag and check is size is been added
+    if size: 
+        # quantity is 0 gets updated accordingly
+        if quantity > 0:
+            # if there is a size it gets updated from the dictionary
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # del if quantity is 0
+            del bag[item_id]['items_by_size'][size]
+    #else if item has no size run orginal         
+    else:        
+        # if no size and it get removed
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop{item_id}
     # overwrite the variable if it doesnt exisit
     request.session['bag'] = bag
-    # redirect bag ot view bag url using the reverse
+    # redirect to the bag url
     return redirect(reverse('view_bag'))
