5ac2622b16a056e4e6ee3efff4acd22f0e90b1fe
Colin Shaw <colin.shaw400@outlook.com>
2023-07-06 19:38:29 +0000

    Add view and urls for removing items in shopping bag

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
-    path('update/<item_id>/', views.update_bag, name='update_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<item_id>/', views.remove_item, name='remove_item'),
 ]
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -43,8 +43,8 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
-# view update product quantity in the shopping bag
-def update_bag(request, item_id):
+ # view update product quantity in the shopping bag
+def adjust_bag(request, item_id):
     """ update quantity of a product to the shopping bag"""
     # get quanity and convert it to an integar as it is a string
     quantity = int(request.POST.get('quantity'))
@@ -65,6 +65,8 @@ def update_bag(request, item_id):
         else:
             # del if quantity is 0
             del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['item_by_size']:
+                bag.pop(item_id)
     #else if item has no size run orginal         
     else:        
         # if no size and it get removed
@@ -76,3 +78,33 @@ def update_bag(request, item_id):
     request.session['bag'] = bag
     # redirect to the bag url
     return redirect(reverse('view_bag'))
+
+# view remove product quantity in the shopping bag
+def remove_item(request, item_id):
+    """ remove quantity of a product to the shopping bag"""
+    # add try block to return 200 error if code does not execure
+    try:
+        # set size to none, no need for quantity as it set to zero
+        size = None
+        #if statement for product size in request . post it gets set to that
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        # stores shopping bag in the sessions so it is not lost till the session is closed
+        bag = request.session.get('bag', {})
+
+        # if statment for sturcture of bag and check is size is been added
+        if size:
+            # del if quantity is 0
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['item_by_size']:
+                bag.pop(item_id)
+        #else if item has no size run orginal         
+        else:        
+            # if no size and pop it out of the bag
+                bag.pop(item_id)
+        # overwrite the variable if it doesnt exisit
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    #catch status 500 error. e will return error to template if anythign goes wrong 
+    except Exception as e:
+        return HttpResponse(status=500)
