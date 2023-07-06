41fae4b5c18d0c3419783268ddf4c9fa2575eeb5
Colin Shaw <colin.shaw400@outlook.com>
2023-07-06 17:27:25 +0000

    Add updates for context processor for if the item has a size and ignore if it doesnt

--- shoppingbag/contexts.py
+++ shoppingbag/contexts.py
@@ -3,9 +3,8 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-# makes all dictioanries avilalble to all templeates on the app
+# makes all dictioanries avialble to all templeates on the app
 def bag_contents(request):
-    
     # setting bag items to an empty list
     bag_items = []
     # total set to 0
@@ -13,23 +12,39 @@ def bag_contents(request):
     # count set to 0
     product_count = 0
     # accessing the bag in session if it exists
-    bag = request.session.get('bag',{})
+    bag = request.session.get('bag', {})
 
-    # for loop for each item and quantity in the bag to add to bag
-    for item_id, quantity in bag.items():
-        # getting the product
-        product = get_object_or_404(Product, pk=item_id)
-        # add price and quantity to total
-        total += quantity * product.price
-        # increments the product count by the quantity
-        bag_items.append(({
-            # dictonary added to bag
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        }))
-
-
+    # for loop for each item and quanity in the bag to add to bag
+    # from course content
+    # add item_data to use all data not just quantity
+    for item_id, item_data in bag.items():
+        # if statment for code to be executed if it only has sizes.
+        # if item is an integer it knows to use quantity
+        if isinstance(item_data, int):
+            # getting the product
+            product = get_object_or_404(Product, pk=item_id)
+            # add price and quantity to total
+            total += item_data * product.price
+            product_count += item_data
+            # incremnts the product count by the quantity
+            bag_items.append({
+                # dictonary added to bag
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        # else it is a dictionary and it has to itterate accordingly through
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     # calculation for free shipping and adding on cost of shipping
     if total < settings.FREE_DELIVERY_THRESHOLD:
@@ -39,7 +54,7 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
 
     # returns a dictionary as context processer
