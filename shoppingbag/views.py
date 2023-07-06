43f89a46ba5068cf392d33e7186fc752d3178c04
Colin Shaw <colin.shaw400@outlook.com>
2023-07-06 17:15:39 +0000

    Add a view for sizes in the add to bag view using django logic if the size exists or not

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,27 +1,44 @@
 from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
-
 def view_bag(request):
     """ A view to return the bag content page"""
 
     return render(request, 'bag/bag.html')
 
-# add in add to bag view 
+# add in add to bag view
 def add_to_bag(request, item_id):
     """Add quanity of a product to the shopping bag"""
     # get quanity and convert it to an integar as it is a string
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    # set size to none
+    size = None
+    #if statement for product size in request . post it gets set to that
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     # stores shopping bag in the sessions so it is not lost till the session is closed
     bag = request.session.get('bag', {})
 
-    # add item to bag or update the bag quanity if already in bag
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
-    
+    # if statment for sturcture of bag and check is size is been added
+    if size: 
+        # dictionary for size using key work item by sizefor multiple sizes
+        if item_id in list(bag.keys()):
+            # check two items of same id and size exist and set it equal to quantity
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    #else if item has no size run orginal         
+    else:        
+        # add item to bag or update the bag quanity if already in bag
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
     # overwrite the variable if it doesnt exisit
     request.session['bag'] = bag
     return redirect(redirect_url)
