04024582362763803d7d94cae897b3d8af8b3dfd
Colin Shaw <colin.shaw400@outlook.com>
2023-07-07 18:46:15 +0000

    Add function for updating the total after line item is added and caulculate the delivery charge

--- checkout/models.py
+++ checkout/models.py
@@ -27,12 +27,25 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
 
-    # function to return a random number
+    # returns the random order number
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
+    
+    # function to update the total and update delivery cost
+    def update_total(self):
+        """
+        Update grand total each time a line item is added accounting for delivery costs
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
 
     #   overwrites the default save method and returns random number fromm
     #   generate number function
