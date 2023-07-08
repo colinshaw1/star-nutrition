7b3f47709625eaa24b1a9ca0a9c445cb2ba5a41b
Colin Shaw <colin.shaw400@outlook.com>
2023-07-08 15:42:59 +0000

    Add checkour view the view calls a bag session and renders a message if the bag is empty

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+from .forms  import OrderForm
 
 # Create your views here.
+# checkout view
+def checkout(request):
+    # get the bag from the session
+    bag = request.session.get('bag', {})
+    # if statment for logic around if bag is empty
+    if not bag:
+        messages.error(request, "Your bag is currently empty")
+        return redirect(reverse('products'))
+
+    # creating the order form instance
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    # renders out the order form
+    return render(request, template, context)
