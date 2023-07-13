03209ddae4a121b5b5053f3803982f4d13bfedb0
Colin Shaw <colin.shaw400@outlook.com>
2023-07-13 17:14:14 +0000

    Add elements to stripe JS file for billing and shipping details

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -55,11 +55,40 @@ form.addEventListener('submit', function(ev) {
     // disaplys card element to prevern loads of submissions
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    // js to run the overlay when processing the payment
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     // billing details by calling to stripe
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
-        }
+            // add billing info for webhooks
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+                address:{
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    state: $.trim(form.county.value),
+                }
+            }
+        },
+        // same as billing but takes in delivery info
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.town_or_city.value),
+                country: $.trim(form.country.value),
+                postal_code: $.trim(form.postcode.value),
+                state: $.trim(form.county.value),
+            }
+        },
         // if error post message
     }).then(function(result) {
         if (result.error) {
@@ -70,6 +99,9 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            // js to run the overlay when processing the payment
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
