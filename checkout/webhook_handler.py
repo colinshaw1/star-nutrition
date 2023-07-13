ac9b625aefed98a8c814f8da6cd744cf2c3c78ba
Colin Shaw <colin.shaw400@outlook.com>
2023-07-13 15:09:33 +0000

    Add webhook for failure and success attempts

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -14,3 +14,19 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
+    # handle payment intent success
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+    # handle payment intent faliure
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
