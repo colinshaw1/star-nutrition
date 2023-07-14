ce2d651687df1704007951bc33ed9d2d170f5b9e
Colin Shaw <colin.shaw400@outlook.com>
2023-07-14 17:32:28 +0000

    Add imports to webhook handler file

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from .models import Order, OrderLineItem
+from products.models import Product
+
+import json
+import time
 
 # handles webhooks to stop early checkout cancelations
 class StripeWH_Handler:
