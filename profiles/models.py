9fd9877d13dedea618cef4a7f6b7f6be23eb916a
Colin Shaw <colin.shaw400@outlook.com>
2023-07-15 11:47:47 +0000

    Add a receiver for when user saves thier details

--- profiles/models.py
+++ profiles/models.py
@@ -24,3 +24,13 @@ class UserProfile(models.Model):
     # return username string
     def __str__(self):
         return self.user.username
+# receiver to save user profiles details to file
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
