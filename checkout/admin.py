334cf3cf54fd945f7c8a73da147547afe3961b80
Colin Shaw <colin.shaw400@outlook.com>
2023-07-15 16:30:28 +0000

    Add update to order admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,7 +11,7 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
     # read only fields so they can not be update and change the order
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
