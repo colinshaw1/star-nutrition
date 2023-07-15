195b69493ca84b2bb7113dd310fa5f6c32925722
Colin Shaw <colin.shaw400@outlook.com>
2023-07-15 16:28:49 +0000

    Fix urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # url to link the proile view
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
     # view for order history
-     path('order_history/<order_number>', views.order_history, name='order_history')
+    path('order_history/<order_number>', views.order_history, name='order_history'),
+]
