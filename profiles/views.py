f47bd73c3b9e82d2aa0805bede165c473215bdab
Colin Shaw <colin.shaw400@outlook.com>
2023-07-15 16:23:47 +0000

    Add order history view and css for order histrory

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 # Create your views here.
 # create profile view returns html template
 def profile(request):
@@ -24,6 +24,23 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+# view for order history
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+    # message to show user is looking at a past order
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+    # url to template checkout success
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
